4fbe09d84895092383b710e4b937dcdf27da51f2
Krrishjaiswal <krrishjais45678@gmail.com>
2026-01-05 08:44:44 +0000

    Added-Day-24-Learning

---
+++ JS-Basics-Practice-Sheet/script.js
@@ -0,0 +1,15 @@
+// Print your name
+let name = prompt("Enter your name");
+console.log(name);
+
+// Add two numbers
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter second number"));
+
+let sum = num1 + num2;
+console.log("Sum is: " + sum);
+
+// Square of a number
+let num = Number(prompt("Enter your number"));
+let Square = 2*num;
+console.log("Square is:"+ Square );
